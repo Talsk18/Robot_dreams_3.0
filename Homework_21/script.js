bca74dba55eadf39e2fc17c43b0f8866ce0cec60
Talsk14 <nikitatalskyyy@gmail.com>
2023-07-07 22:15:48 +0300

    Commit

---
+++ Homework_21/script.js
@@ -0,0 +1,12 @@
+$(function () 
+{
+    let first = +prompt("Enter first number");
+    let second = +prompt("Enter second number");
+
+    let a = first + second;
+    let b = first - second;
+    let c = first / second;
+    let d = first * second;
+
+    alert(a + " " + b + " " + c + " " + d);
+});
