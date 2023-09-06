a24475eeca6fe0c12eed16f23ff77b86f192dded
Talsk18 <138514326+Talsk18@users.noreply.github.com>
2023-09-07 00:24:09 +0200

    Update func.test.js

--- Homework_30/func.test.js
+++ Homework_30/func.test.js
@@ -5,7 +5,7 @@ let accumulator = new functions.Accumulator(20);
 
 describe('Several tests', function () {
     it('checkArray', function () {
-        expect(functions.checkArray(a, isPassed)).toBeDefined();
+        expect(functions.checkArray(a, isPassed)).toBe(125);
     });
 
     it('bubbleSort', function () {
