e9c54faf86f0dca7f074c922f558ce96d65da11f
Talskyy114 <nikitanikitovic02@gmail.com>
2023-09-07 16:01:56 +0200

    Homework_30_5.0

--- Homework_30/func.test.js
+++ Homework_30/func.test.js
@@ -1,10 +1,13 @@
 const functions = require("./func");
 let a = new Array(13, 22, 31, 4, 5, 16, 7, 8, 9, 10);
 let isPassed = true;
+
 let accumulator = new functions.Accumulator(20);
 let spyLogger = jest.spyOn(functions, 'triangle');
+
 functions.triangle();
 functions.triangle();
+
 describe('Several tests', function () {
     it('checkArray', function () {
         expect(functions.checkArray(a, isPassed)).toBe(125);
